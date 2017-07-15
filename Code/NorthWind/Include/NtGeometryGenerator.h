088c4b7cfbea31d010c326c8bf28372814eff0d2
jack <jack@twohandsgames.com>
2017-07-15 09:42:40 +0900

    .

--- Code/NorthWind/Include/NtGeometryGenerator.h
+++ Code/NorthWind/Include/NtGeometryGenerator.h
@@ -1,1 +1,42 @@
 #pragma once
+
+namespace nt {  namespace renderer
+
+class NtGeometryGenerator
+{
+public:
+    struct Vertex
+    {
+        Vertex() {}
+        Vertex(const XMFLOAT3& p, const XMFLOAT3& n, const XMFLOAT3& t, const XMFLOAT2 uv)
+            : Position(p), Normal(n), TangentU(t), TexC(uv)
+        {
+        }
+        Vertex(ntFloat px, ntFloat py, ntFloat pz,
+            ntFloat nx, ntFloat ny, ntFloat nz,
+            ntFloat tx, ntFloat ty, ntFloat tz,
+            ntFloat u, ntFloat v)
+            : Position(px, py, pz),
+            , Normal(nx, ny, nz)
+            , TangentU(tx, ty, tz)
+            , TexC(u, v)
+        {
+        }
+        
+        XMFLOAT3 Position;
+        XMFLOAT3 Normal;
+        XMFLOAT3 TangentU;
+        XMFLOAT2 TexC;
+    }
+
+    struct MeshData
+    {
+        NtArray<Vertex> Vertices;
+        NtArray<ntUint> Indices;
+    }
+
+public:
+    void CreateGrid(ntFloat width, ntFloat depth, ntUint m, ntUint n);
+}
+
+}}
--- Code/NorthWind/Source/NtGeometryGenerator.cpp
+++ Code/NorthWind/Source/NtGeometryGenerator.cpp
@@ -3,3 +3,15 @@
 
 #include "NtGeometryGenerator.h"
 
+void NtGeometryGenerator::CreateGrid(ntFloat width, ntFloat depth, ntUint m, ntUint n)
+{
+    ntUint vertexCount = m * n;
+    ntUint faceCount = (m - 1) * (n - 1) * 2;
+
+    // making vertices
+    ntFloat dx = width / (n - 1);
+    ntFloat dz = depth / (m - 1);
+
+    ntFloat du = 1.0f / (n - 1);
+    ntFloat dv = 1.0f / (m - 1);
+}
